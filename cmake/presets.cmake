060a7699ac9027f9ffdb1d30f9742438b671ea39
CodingWithMagga <codingwithmagga@googlemail.com>
2023-10-08 10:12:53 +0200

    Fix github action errors #26

--- cmake/presets.cmake
+++ cmake/presets.cmake
@@ -19,10 +19,13 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 set(QML_IMPORT_PATH ${CMAKE_BINARY_DIR}/libs/mva_gui CACHE STRING "" FORCE)
 
 if (MSVC)
-    enable_cxx_compiler_flag_if_supported("/W4")
+    enable_cxx_compiler_flag_if_supported("/Wall")
     enable_cxx_compiler_flag_if_supported("/WX")
     enable_cxx_compiler_flag_if_supported("/external:anglebrackets")
     enable_cxx_compiler_flag_if_supported("/external:W0")
+
+    # TODO: Find a way to make this path independent
+    enable_cxx_compiler_flag_if_supported('/external:I "D:\\a\\mathvizanimator\\mathvizanimator\\Qt\\6.5.1\\msvc2019_64\\include" ')
 else()
     enable_cxx_compiler_flag_if_supported("-Wall")
     enable_cxx_compiler_flag_if_supported("-Werror")
