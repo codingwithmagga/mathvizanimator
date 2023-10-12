8773a6af5fe7da9f935f262e5f88fb8087afe2ae
CodingWithMagga <codingwithmagga@googlemail.com>
2023-10-12 13:34:50 +0200

    Add missing files & change github action workflow

---
+++ libs/mva_workflow/include/workflow/itemhandler.h
@@ -0,0 +1,51 @@
+#ifndef ITEMHANDLER_H
+#define ITEMHANDLER_H
+
+#include <QObject>
+#include <QQuickItem>
+#include <QStandardItemModel>
+
+#include "abstractitem.h"
+#include "workflow_global.h"
+
+class WORKFLOW_EXPORT ItemHandler : public QObject
+{
+    Q_OBJECT
+public:
+    enum ItemRoles { QUICKITEM = Qt::UserRole + 1 };
+
+    explicit ItemHandler(QObject *parent = nullptr);
+
+    qsizetype numItems() const { return m_itemmodel.rowCount(); }
+    QStandardItemModel *model() { return &m_itemmodel; }
+    QList<QQuickItem *> items();
+
+public slots:
+    void clear();
+    void addItem(QQuickItem *const quick_item);
+    void removeItem(QQuickItem *const quick_item);
+
+private:
+    struct ItemExtract
+    {
+        AbstractItem *item;
+        bool error;
+    };
+
+    bool itemAlreadyExists(QQuickItem *const quick_item);
+    bool itemNameAlreadyExists(const QString &name);
+    ItemExtract extractAbstractItem(QQuickItem *const quick_item);
+
+    QString prepareNewItemName(const QString &old_item_name);
+
+    QStandardItemModel m_itemmodel;
+};
+
+class ItemModelItem : public QStandardItem
+{
+public:
+    explicit ItemModelItem(const QString &text);
+    ~ItemModelItem();
+};
+
+#endif // ITEMHANDLER_H
