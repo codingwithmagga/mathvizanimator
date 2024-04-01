3f7f4326cfb9d655d66f968785adcb65a79e1ebf
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-01 12:40:34 +0200

    Using doxysphinx

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 if read_the_docs_build:
 	input_dir = '../libs'
-	output_dir = 'build'
+	output_dir = 'mathvizanimator'
 	configureDoxyfile(input_dir, output_dir)
 	subprocess.call('doxygen', shell=True)
 	breathe_projects['MathVizAnimator'] = output_dir + '/xml'
