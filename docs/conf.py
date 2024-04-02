c8e56ef4edded95fb6a5fe871d3072a7f6951703
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-02 14:36:13 +0200

    try different way [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/html/**.html']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*.html']
 
 
 # -- Options for HTML output -------------------------------------------------
