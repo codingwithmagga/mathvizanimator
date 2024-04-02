c66eaeceebfd0d3354a686989fe03673a7bd0cc7
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-02 11:57:20 +0200

    Fix pathing issue [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ if read_the_docs_build:
     read_the_docs_build_folder = Path(os.environ.get('READTHEDOCS_OUTPUT', None))
     main_folder = read_the_docs_build_folder.parent.absolute()
     input_dir = '../libs'
-    output_dir = main_folder / 'docs'
+    output_dir = main_folder / 'docs' / 'mathvizanimator'
     configureDoxyfile(input_dir, output_dir.absolute().as_posix())
     subprocess.call('doxygen', shell=False)
     subprocess.call(['doxysphinx', 'build', '.', read_the_docs_build_folder, output_dir / 'html'], shell=False)
@@ -69,8 +69,8 @@ autosectionlabel_prefix_document = True
 if read_the_docs_build:
     read_the_docs_build_folder = Path(os.environ.get('READTHEDOCS_OUTPUT', None))
     main_folder = read_the_docs_build_folder.parent.absolute()
-    output_dir = main_folder / 'docs'
-    doxygen_root = output_dir.absolute().as_posix()
+    output_dir = main_folder / 'docs' / 'mathvizanimator'
+    doxygen_root = output_dir.relative_to('.').as_posix()
     doxylink = {
         "mva": ( 
             f"{doxygen_root}/html/tagfile.xml", 
--- docs/user_guide/index.rst
+++ docs/user_guide/index.rst
@@ -10,5 +10,6 @@ The user guide shows how to use the features of the MathVizAnimator application.
     project_settings
     latex_items
     bugs_features
+    /docs/doxygen/demo/html
 
 :mva:`AbstractItem`
