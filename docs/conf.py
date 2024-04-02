f3c240e9715b3171117b872b42d74fb48af7ae38
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-02 12:29:46 +0200

    Fixing paths all day long

--- docs/conf.py
+++ docs/conf.py
@@ -67,10 +67,10 @@ extensions = ["breathe",
 autosectionlabel_prefix_document = True
 
 if read_the_docs_build:
-    read_the_docs_build_folder = PurePosixPath(os.environ.get('READTHEDOCS_OUTPUT', None))
+    read_the_docs_build_folder = Path(os.environ.get('READTHEDOCS_OUTPUT', None))
     main_folder = read_the_docs_build_folder.parent.absolute()
     output_dir = main_folder / 'docs' / 'mathvizanimator'
-    doxygen_root = output_dir.relative_to(read_the_docs_build_folder, walk_up=True).as_posix()
+    doxygen_root = PurePosixPath(output_dir.as_posix()).relative_to(read_the_docs_build_folder, walk_up=True).as_posix()
     doxylink = {
         "mva": ( 
             f"{doxygen_root}/html/tagfile.xml", 
