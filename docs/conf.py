5f3f01a105cd1ee8d029761d3d59550ae55c8695
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-13 22:03:08 +0200

    Test different path [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -73,15 +73,15 @@ extensions = ["sphinx.ext.graphviz",
 autosectionlabel_prefix_document = True
 
 if read_the_docs_build:
-    doxygen_root = os.path.relpath(get_output_dir().absolute().as_posix(), start=read_the_docs_build_folder)
+    doxygen_root = os.path.relpath(read_the_docs_build_folder , start=read_the_docs_build_folder)
     doxylink = {
         "mva": ( 
             f"{doxygen_root}/html/tagfile.xml", 
-            f"{doxygen_root}/html"
+            "mathvizanimator/html"
 		)
 	}
 else:
-    doxygen_root = "mathvizanimator" # this is just a convenience variable
+    doxygen_root = "mathvizanimator" 
     doxylink = {
 		"mva": ( 
 			f"{doxygen_root}/html/tagfile.xml", # the first parameter of this tuple is the tagfile
