cca6131fff46d41f67e73e3c6f2e9fb6d4b8eabb
CodingWithMagga <codingwithmagga@googlemail.com>
2024-04-24 15:07:11 +0200

    Refactor itemhandler using propertymodel::appendProperty

--- libs/mva_workflow/src/itemhandler.cpp
+++ libs/mva_workflow/src/itemhandler.cpp
@@ -274,7 +274,7 @@ void ItemHandler::removeAnimation(const qint32 animation_number)
 // TODO(codingwithmagga): Refactor this function, give useful var names
 void ItemHandler::appendProperties(const auto obj, auto meta_object, const QStringList& allowedProperties)
 {
-    QList<std::pair<QString, QVariant>> property_list;
+    QList<ItemProperty> property_list;
 
     for (auto i = meta_object->propertyOffset(); i < meta_object->propertyCount(); ++i) {
         if (allowedProperties.contains(QString(meta_object->property(i).name()))) {
@@ -283,10 +283,7 @@ void ItemHandler::appendProperties(const auto obj, auto meta_object, const QStri
     }
 
     for (auto& property : property_list) {
-        auto stdItemName(new QStandardItem(property.first));
-        auto stdItemValue(new QStandardItem(property.second.toString()));
-
-        m_property_model.appendRow(QList<QStandardItem*> { stdItemName, stdItemValue });
+        m_property_model.appendProperty(property);
     }
 }
 
